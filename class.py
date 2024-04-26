61826d390fdbef558e98223ff2608c98c929e115
SuperiorGitTele <semovita56@gmail.com>
2024-04-26 17:25:48 +0100

    my car

---
+++ class.py
@@ -0,0 +1,109 @@
+
+# x = 5
+# y =10
+# print(x+y)
+
+# first_name = input("Enter your first name\n")
+# second_name = input("Enter your second name:\n")
+# print(first_name + second_name)
+# concartination
+# print("my name is" +  ' ' + first_name + ' ' + second_name)
+# print(f'My name is {first_name} {second_name}')
+
+# num1 = int(input("Enter 1st number:\n"))
+# num2 = int(input("Enter 2nd number:\n"))
+
+# print(f'{num1} + {num2} = {num1 + num2}')
+
+
+# my_string = "What do you want to do in life?"
+# print(my_string[0:5:2])
+# print(my_string[::-1])
+# print(my_string[0::2])
+
+# new_string = my_string.split()
+# new_string.reverse()
+# print(new_string)
+
+#list
+# colors = ['blue', 'green', 'red', 'purple']
+# print(colors)
+
+# for i in colors:
+    # print(i[0])
+    # print(i.upper())
+
+# for emeka in range(0, 11, 2):
+#     print(emeka)
+
+# employees = [
+#     {
+#     "name":'prosper',
+#     "occupation": ["python developer", "Data science"]
+# }
+# ]
+# employees[0]["occupation"]=  "software dev"
+# print(employees)
+
+# my_lost  = ["Banana", "orange", "Apple"]
+# my_lost.sort()
+# if "Banana" in my_lost:
+#     print(True)
+# my_list = ["a", "b", "c"]
+# my_list.reverse()
+# print(my_list)
+
+# my_number = [2, 3, 9, 10]
+# for i in my_number:
+#     print(i ** 2)
+# my_string = "Welcome to digital fortress"
+# print(my_string[8:10])
+
+
+#operators
+# arithmetical operators - + * / % += -= ** >= <= ==
+# num1 = 30
+# num2 = 50
+# print(num1 + num2)
+# print(num1 / num2)
+# print(num1 * num2)
+# print(num1 == num2)
+# print(num1>= num2)
+# print(num2 <= num2)
+# print(num1 % num2)
+# print(num1 ** 2)
+
+# score = input("Enter your score:\n")
+# score = score.upper()
+# if score == "A": 
+#     print("First position")
+# elif score == "B":
+#     print("Second position")
+# elif score == "C":
+#     print("Third position")
+# elif score == 'D':
+#     print('Fail')
+# else:
+#     print("Unknown Result")
+
+#loops
+# x = 0
+# while x <= 11:
+#     print(x)
+#     x += 1
+
+# for i in range(0, 12):
+#     print(i)
+    
+
+
+# import random
+# x = random.randint(0, 5)
+# print(x)
+
+# score = {10, 20, 30, 20, 60, 10}
+# for x in score:
+#     if x > 20:
+#         print(x ** 2)
+#     else:
+#         print("")
